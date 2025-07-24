65d30c502818e859e042f07906c0b746b311dc93
eqbal amini <eqbalamininejad@gmail.com>
2025-07-24 15:04:33 +0330

    add: update bot lang

--- bot/locales/__init__.py
+++ bot/locales/__init__.py
@@ -2,7 +2,7 @@ from importlib import import_module
 
 _cache = {}
 
-def translate(key: str, lang: str = 'ru', **kwargs) -> str:
+def translate(key: str, lang: str = 'fa', **kwargs) -> str:
     if lang not in _cache:
         _cache[lang] = import_module(f'.{lang}', __name__).MESSAGES
     text = _cache[lang].get(key, key)
